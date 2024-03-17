e28dfecaee3b128fba698c7be98682b4e802a327
badrobotenergy <badrobotenergy@gmail.com>
2024-03-16 22:30:05 -0700

    Added Cargo.toml with project information
    Renamed LICENSE.md to MIT-LICENSE.md
    In code base made changes to server/src/main.rs
    Imported std library to handle i/o, invoke a tcp listener and stream bytes
    and start a thread when the client binds to the server.
    There is also code to create a buffer to streaming tcp traffic and read bytes from
    that buffer.
    Added logic, if there are zero bytes on attempted connect the client closed the connection
    if not write to the buffer and echo that back to the client
    If there is an error reading the buffer, trap the error and print an error message on the
    server side
    
    BREAK: The code isn't cannot compile at this stage
    
    FIX: N/A

---
+++ server/src/main.rs
@@ -0,0 +1,19 @@
+use std::io::(Read, Write);             // import std library input/output read and write
+use std::net::(TcpListener, TcpStream); // import std library tcp listener and streaming
+use std::thread;                        // import std library thread, will spawn when tcp connection "binds"
+
+fn handle_client(mut stream: TcpStream);
+    let mut buf = [0, 512];                                     // create buffer
+    loop (
+        match stream.read(&mut buf) {                           // read bytes from buffer
+            ok(bytes_read) => (
+                if bytes_read == 0 ( return; )                  // zero bytes, client closed the connexction
+                stream.write(&buf[0..bytes_read]).unwrap();     // echo back to the client
+            ),
+            Err(e) =>(                                          // trap error 
+                eprintln!("Failed to read from client: {}", e); // print error messaage
+                return;
+            )
+        )
+    )
+)
